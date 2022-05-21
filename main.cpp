a95b3c1ae4c1b43e91684e4d5b6e59e250a7f48a
JAGS1087 <98847793+JAGS1087@users.noreply.github.com>
2022-05-21 01:03:53 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,195 @@
+/*
+╔══════════════════════════════════════════════════════════╗
+║                       Credits                            ║
+║  Author/Main Dev:                                        ║
+║  Helper/Secandary Dev :                                  ║
+║  Grahics:                                                ║
+║                                                          ║
+║                                                          ║
+╚══════════════════════════════════════════════════════════╝
+*/
+
+// Include 
+
+#include <iostream>
+/*
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include <iomanip>
+#include <math.h>
+#include <string>
+#include <thread>
+*/
+
+// Using Namespace
+
+using namespace std;
+/*
+using namespace std::this_thread;
+using namespace std::chrono;
+*/
+
+// User defined Variables
+
+/*
+const [variable] [name] = [num];
+
+struct [name] 
+{
+  
+}
+
+class [name] 
+{
+  
+}
+
+void codeSlowDown1Sec() { sleep_for(seconds(1)); }
+void codePause() { getchar(); }
+void screenClear() { system("clear"); }
+
+void semiAnimatedLoadingScreen();
+
+*/
+
+int main()
+{
+  cout << "e" << endl;
+}
+
+/*
+void semiAnimatedLoadingScreen() 
+{
+
+// Face 1
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║░░░░░░░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "║░░░░░░░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+  
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 2
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║██░░░░░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "║██░░░░░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 3
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║████░░░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "║████░░░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 4
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║██████░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "║██████░░░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 5
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║████████░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "║████████░░░░░░░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 6
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║██████████░░░░░░░░░░░░░░░║" << endl;
+  cout << "║██████████░░░░░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 7
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║████████████░░░░░░░░░░░░░║" << endl;
+  cout << "║████████████░░░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 8
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║██████████████░░░░░░░░░░░║" << endl;
+  cout << "║██████████████░░░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 9
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║████████████████░░░░░░░░░║" << endl;
+  cout << "║████████████████░░░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 10
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║██████████████████░░░░░░░║" << endl;
+  cout << "║██████████████████░░░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 11
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║████████████████████░░░░░║" << endl;
+  cout << "║████████████████████░░░░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 12
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║███████████████████████░░║" << endl;
+  cout << "║███████████████████████░░║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+
+// Face 13
+  cout << "╔═════════════════════════╗" << endl;
+  cout << "║█████████████████████████║" << endl;
+  cout << "║█████████████████████████║" << endl;
+  cout << "╚═════════════════════════╝" << endl;
+  cout << "          Loading          " << endl;
+
+  codeSlowDown1Sec();
+  screenClear();
+}
+*/
